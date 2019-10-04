2686465ce709b947c8860a2ef61856a8fb382915
oliver-csu <oliver.csu@gmail.com>
2019-10-04 22:53:18 +0800

    MyBatis快速入门工程初始化

---
+++ mybatis-quick-start/src/main/java/ren/oliver/mybatis/quick/start/app/App.java
@@ -0,0 +1,9 @@
+package ren.oliver.mybatis.quick.start.app;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ mybatis-quick-start/src/main/java/ren/oliver/mybatis/quick/start/pojo/User.java
@@ -0,0 +1,5 @@
+package ren.oliver.mybatis.quick.start.pojo;
+
+public class User {
+
+}
