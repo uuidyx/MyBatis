bebcc7c539df703a2a26e53c036b0c587dac6213
oliver-csu <oliver.csu@gmail.com>
2019-10-09 22:46:29 +0800

    Bugfix

---
+++ mybatis-learning/src/test/java/ren/oliver/mybatis/learning/mapper/UserMapperTest.java
@@ -0,0 +1,30 @@
+package ren.oliver.mybatis.learning.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import ren.oliver.mybatis.learning.pojo.User;
+
+import java.io.InputStream;
+
+public class UserMapperTest {
+
+    @Test
+    public void selectByPrimaryKeyTest() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 读取MyBatis配置文件创建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        inputStream.close();
+        // 获取SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 获取对应mapper
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        // 执行查询语句并返回结果
+        User user = mapper.selectByPrimaryKey(1);
+        System.out.println(user.toString());
+    }
+
+}
