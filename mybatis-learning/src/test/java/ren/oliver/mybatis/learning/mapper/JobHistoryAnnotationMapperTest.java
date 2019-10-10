8295c752020328bc9b6e5a81aac3b1820ed6ed34
oliver-csu <oliver.csu@gmail.com>
2019-10-10 11:30:05 +0800

    代码简化

--- mybatis-learning/src/test/java/ren/oliver/mybatis/learning/mapper/JobHistoryAnnotationMapperTest.java
+++ mybatis-learning/src/test/java/ren/oliver/mybatis/learning/mapper/JobHistoryAnnotationMapperTest.java
@@ -6,22 +6,32 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class JobHistoryAnnotationMapperTest {
 
-    @Test
-    public void selectByUserIdTest() throws Exception {
+    SqlSessionFactory sqlSessionFactory;
+
+    SqlSession sqlSession;
+
+    @Before
+    public void init() throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
-        // 读取MyBatis配置文件创建SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        // 1.读取mybatis配置文件创SqlSessionFactory
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         inputStream.close();
         // 获取SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @Test
+    public void selectByUserIdTest() {
         // 获取对应mapper
         JobHistoryAnnotationMapper jobHistoryAnnotationMapper =
                 sqlSession.getMapper(JobHistoryAnnotationMapper.class);
@@ -31,14 +41,7 @@ public class JobHistoryAnnotationMapperTest {
     }
 
     @Test
-    public void selectAllTest() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        // 读取MyBatis配置文件创建SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        inputStream.close();
-        // 获取SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+    public void selectAllTest() {
         // 获取对应mapper
         JobHistoryAnnotationMapper jobHistoryAnnotationMapper =
                 sqlSession.getMapper(JobHistoryAnnotationMapper.class);
@@ -48,14 +51,7 @@ public class JobHistoryAnnotationMapperTest {
     }
 
     @Test
-    public void insertTest() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        // 读取MyBatis配置文件创建SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        inputStream.close();
-        // 获取SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+    public void insertTest() {
         // 获取对应mapper
         JobHistoryAnnotationMapper jobHistoryAnnotationMapper =
                 sqlSession.getMapper(JobHistoryAnnotationMapper.class);
