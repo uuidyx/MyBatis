6335facb8deff4574fa77f4b70551097c3895a68
oliver-csu <oliver.csu@gmail.com>
2019-10-10 11:32:09 +0800

    代码简化

--- mybatis-quick-start/src/test/java/ren/oliver/mybatis/quick/start/mapper/UserMapperTest.java
+++ mybatis-quick-start/src/test/java/ren/oliver/mybatis/quick/start/mapper/UserMapperTest.java
@@ -6,21 +6,30 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
 
 public class UserMapperTest {
 
-    @Test
-    public void selectByPrimaryKeyTest() throws Exception {
+    SqlSessionFactory sqlSessionFactory;
+
+    SqlSession sqlSession;
+
+    @Before
+    public void init() throws Exception {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         // 读取MyBatis配置文件创建SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         inputStream.close();
         // 获取SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @Test
+    public void selectByPrimaryKeyTest() throws Exception {
         // 获取对应mapper
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         // 执行查询语句并返回结果
